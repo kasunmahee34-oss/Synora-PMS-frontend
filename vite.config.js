6b77449df1548820f15cbf2f98053464148bb7ce
kasunmahee34-oss <kasunmahee34@gmail.com>
2026-08-28 07:29:51 -0700

    url change

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-      //  target: 'http://localhost:5000',
+        //target: 'http://localhost:5000',
        target: 'https://synora-pms-backend-production.up.railway.app',
         changeOrigin: true,
         secure: false,
